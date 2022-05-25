4cdc2a553df20f290ddad4855afda9eaa243d9ce
sendist <sendisetiawan10902@gmail.com>
2022-05-25 19:46:52 +0700

    upload source code W13

---
+++ Praktikum-Tekpro-W13/src/com/polban/tekpro/monopoly/GoCell.java
@@ -0,0 +1,13 @@
+package com.polban.tekpro.monopoly;
+
+public class GoCell extends Cell {
+	public GoCell() {
+		super.setName("Go");
+	}
+
+	public void playAction() {
+	}
+	
+	void setName(String name) {
+	}
+}
